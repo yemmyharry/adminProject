251456f6936fc55b8135d92d814527cf7c038749
Yemi Harry <decagon@Yemi-Harry.local>
2022-01-16 18:49:02 +0100

    add db connect and automigrate functions

---
+++ src/database/db.go
@@ -0,0 +1,23 @@
+package database
+
+import (
+	"adminProject/src/models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	var err error
+	DB, err = gorm.Open(mysql.Open("root:root@tcp(db:3306)/ambassador"), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+}
+
+func AutoMigrate() {
+	DB.AutoMigrate(models.User{})
+}
