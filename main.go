4c9ca248535666d225a1e2458869e16e135cd23f
Yemi Harry <decagon@Yemi-Harry.local>
2022-01-17 01:08:35 +0100

    add route setup

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"adminProject/routes"
 	"adminProject/src/database"
 	"github.com/gofiber/fiber/v2"
 )
@@ -11,6 +12,8 @@ func main() {
 	database.AutoMigrate()
 
 	app := fiber.New()
+
+	routes.SetUp(app)
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
