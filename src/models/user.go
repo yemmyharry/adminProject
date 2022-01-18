ab2a12b7ecc0dee9de25af198b62c8715214b5ab
yemmyharry <yemmyharry@gmail.com>
2022-01-18 21:41:02 +0100

    refactored struct

--- src/models/user.go
+++ src/models/user.go
@@ -1,11 +1,10 @@
 package models
 
 type User struct {
-	ID        int64  `json:"id"`
-	FirstName string `json:"firstname"`
-	LastName  string `json:"lastname"`
-	Email     string `json:"email"`
-	Password  []byte `json:"password"`
-	//ConfirmPassword []byte `json:"confirm_password"`
-	IsAmbassador bool `json:"is_ambassador"`
+	ID           int64  `json:"id"`
+	FirstName    string `json:"firstname"`
+	LastName     string `json:"lastname"`
+	Email        string `json:"email" gorm:"unique`
+	Password     []byte `json:"password"`
+	IsAmbassador bool   `json:"-"`
 }
