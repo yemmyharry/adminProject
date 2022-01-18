c4ed0ff3207a96baca852f4ad02a5c9313d22d91
Yemi Harry <decagon@Yemi-Harry.local>
2022-01-18 15:54:32 +0100

    add login controller

--- controllers/authController.go
+++ controllers/authController.go
@@ -1,11 +1,83 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"adminProject/src/database"
+	"adminProject/src/models"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
+	"time"
+)
 
 func Register(c *fiber.Ctx) error {
 	var data map[string]string
 
+	if err := c.BodyParser(&data); err != nil {
+		return c.Status(503).JSON(fiber.Map{"message": "Error parsing body"})
+	}
+
+	if data["password"] != data["confirm_password"] {
+		return c.Status(503).JSON(fiber.Map{"message": "Passwords do not match"})
+	}
+
+	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), bcrypt.DefaultCost)
+
+	user := models.User{
+		FirstName: data["first_name"],
+		LastName:  data["last_name"],
+		Email:     data["email"],
+		Password:  password,
+		//ConfirmPassword: data["confirm_password"],
+		IsAmbassador: false,
+	}
+
+	database.DB.Create(&user)
+
+	return c.JSON(user)
+}
+
+func Login(c *fiber.Ctx) error {
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return c.Status(503).JSON(fiber.Map{"message": "Error parsing body"})
+	}
+
+	var user models.User
+
+	database.DB.Where("email = ?", data["email"]).First(&user)
+
+	if user.ID == 0 {
+		return c.Status(503).JSON(fiber.Map{"message": "User not found"})
+	}
+
+	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil {
+		return c.Status(503).JSON(fiber.Map{"message": "Invalid password"})
+	}
+
+	payload := jwt.StandardClaims{
+		Subject:   string(user.ID),
+		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+	}
+
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, payload).SignedString([]byte("secret"))
+
+	if err != nil {
+		c.Status(fiber.StatusBadRequest)
+		c.JSON(fiber.Map{"message": "Error creating token"})
+	}
+
+	// send the token as a cookie
+	cookie := fiber.Cookie{
+		Name:     "token",
+		Value:    token,
+		Expires:  time.Now().Add(time.Hour * 24),
+		HTTPOnly: true,
+	}
+
+	c.Cookie(&cookie)
+
 	return c.JSON(fiber.Map{
-		"message": "Registered successfully",
+		"message": "Successfully logged in",
 	})
 }
