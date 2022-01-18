27ae753c6cd9e01d3b713731a3d34f4ec9ac9b47
Yemi Harry <decagon@Yemi-Harry.local>
2022-01-18 15:57:30 +0100

    made chane to user struct

--- src/models/user.go
+++ src/models/user.go
@@ -1,10 +1,11 @@
 package models
 
 type User struct {
-	ID           int64  `json:"id"`
-	FirstName    string `json:"firstname"`
-	LastName     string `json:"lastname"`
-	Email        string `json:"email"`
-	Password     string `json:"password"`
-	IsAmbassador bool   `json:"isambassador"`
+	ID        int64  `json:"id"`
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+	Email     string `json:"email"`
+	Password  []byte `json:"password"`
+	//ConfirmPassword []byte `json:"confirm_password"`
+	IsAmbassador bool `json:"is_ambassador"`
 }
