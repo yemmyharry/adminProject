1ea7afded1b0ebf95e2c0cfe0eaff3f575f5d0fc
yemmyharry <yemmyharry@gmail.com>
2022-01-21 01:31:01 +0100

    create auth middleware

---
+++ middlewares/auth.go
@@ -0,0 +1,23 @@
+package middlewares
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gofiber/fiber/v2"
+)
+
+func IsAuthenticated(c *fiber.Ctx) error {
+	cookie := c.Cookies("token")
+
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte("secret"), nil
+	})
+
+	if err != nil || !token.Valid {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "User not authenticated",
+		})
+	}
+
+	return c.Next()
+}
