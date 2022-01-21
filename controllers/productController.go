86dc519516a12710645bc6d4a1f1df9eb3a275f3
yemmyharry <yemmyharry@gmail.com>
2022-01-21 01:58:06 +0100

    add crud functions for products

--- controllers/productController.go
+++ controllers/productController.go
@@ -20,3 +20,30 @@ func CreateProduct(c *fiber.Ctx) error {
 	database.DB.Create(&product)
 	return c.JSON(product)
 }
+
+func GetProduct(c *fiber.Ctx) error {
+	id := c.Params("id")
+	var product models.Product
+	database.DB.First(&product, id)
+	return c.JSON(product)
+}
+
+func UpdateProduct(c *fiber.Ctx) error {
+	id := c.Params("id")
+	var product models.Product
+	database.DB.First(&product, id)
+	if err := c.BodyParser(&product); err != nil {
+		return c.Status(503).JSON(err)
+	}
+	database.DB.Save(&product)
+	return c.JSON(product)
+}
+
+// DeleteProduct deletes a product
+func DeleteProduct(c *fiber.Ctx) error {
+	id := c.Params("id")
+	var product models.Product
+	database.DB.First(&product, id)
+	database.DB.Delete(&product)
+	return c.JSON(fiber.Map{"status": "successfully deleted"})
+}
