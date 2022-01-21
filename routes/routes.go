ec8c6fabae611cfa8df33138091d5e652e7445a8
yemmyharry <yemmyharry@gmail.com>
2022-01-21 01:37:08 +0100

    add authenticated routes

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"adminProject/controllers"
+	"adminProject/middlewares"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -10,8 +11,15 @@ func SetUp(app *fiber.App) {
 	{
 		api.Post("/admin/register", controllers.Register)
 		api.Post("/admin/login", controllers.Login)
-		api.Get("/admin/user", controllers.User)
-		api.Get("/admin/logout", controllers.Logout)
 
+		authenticatedRoute := api.Use(middlewares.IsAuthenticated)
+		{
+			authenticatedRoute.Get("/admin/user", controllers.User)
+			authenticatedRoute.Post("/admin/logout", controllers.Logout)
+			authenticatedRoute.Put("/admin/update_user", controllers.UpdateUser)
+			authenticatedRoute.Get("/admin/ambassador", controllers.Ambassadors)
+			authenticatedRoute.Get("/admin/products", controllers.Products)
+			authenticatedRoute.Post("/admin/products", controllers.CreateProduct)
+		}
 	}
 }
