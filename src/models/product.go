511767c5abc3188fa603c6562502e3837c9d6544
yemmyharry <yemmyharry@gmail.com>
2022-01-21 01:33:44 +0100

    add product model

---
+++ src/models/product.go
@@ -0,0 +1,9 @@
+package models
+
+type Product struct {
+	ID          int64   `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Image       string  `json:"image"`
+	Price       float64 `json:"price"`
+}
