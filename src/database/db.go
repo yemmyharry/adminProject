14c81826801066bde2769a2a9e3a501da9cef961
yemmyharry <yemmyharry@gmail.com>
2022-01-22 16:08:58 +0100

    added models to automigrate

--- src/database/db.go
+++ src/database/db.go
@@ -19,5 +19,5 @@ func Connect() {
 }
 
 func AutoMigrate() {
-	DB.AutoMigrate(models.User{}, models.Product{})
+	DB.AutoMigrate(models.User{}, models.Product{}, models.Link{}, models.Order{}, models.OrderItem{})
 }
